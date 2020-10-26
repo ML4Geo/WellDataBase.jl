25bccf83ba59ac401ea4e7b3c38dae73273949af
Chris Foster <chris42f@gmail.com>
2020-10-26 23:08:21 +1000

    Greatly improve read speed using DataFrames groupby

--- WellDataBase.jl
+++ WellDataBase.jl
@@ -1,8 +1,10 @@
 module WellDataBase
 
 import DelimitedFiles
-import DataFrames
+using DataFrames: DataFrames, DataFrame, groupby, combine
+using Underscores: @_
 import Dates
+import CSV
 
 csvheader = ["API", "WellName", "Id", "WellId", "ReportDate", "Days", "Lease", "Operator", "WellsInLease", "Field", "Formation", "TotalOil", "LeaseOilAllowable", "WellOilAllowable", "WellOil", "TotalGas", "LeaseGasAllowable", "WellGasAllowable", "WellGas", "TotalWater", "WellWater", "GOR", "ReportMonth", "ReportYear", "ReportedOperator", "ReportedFormation", "InterpretedFormation"]
 csvtypes = [Int64, String, String, String, Dates.Date, Int32, Int32, String, Int32, String, String, Float32, Float32, Float32, Float32, Float32, Float32, Float32, Float32, Float32, Float32, Float32, Int32, Int32, String, String, String]
@@ -72,5 +74,44 @@ function read(datadirs=["csv-201908102241", "csv-201908102238", "csv-20190810223
 
 	return df, api, goodwells, dates
 end
+
+function well_summary(oil, gas, water, report_date)
+    welldates = report_date[.!ismissing.(oil) .|
+                            .!ismissing.(gas) .|
+                            .!ismissing.(water)]
+    (WellOil_sum=sum(skipmissing(oil)),
+     WellGas_sum=sum(skipmissing(gas)),
+     StartDate=isempty(welldates) ? missing : minimum(welldates),
+     EndDate=isempty(welldates) ? missing : maximum(welldates))
+end
+
+function read2(datadirs=["csv-201908102241", "csv-201908102238", "csv-201908102239"]; location="data/eagleford-play-20191008")
+    df = DataFrames.DataFrame()
+    for d in datadirs
+        path = joinpath(location, d, d * "-Production.csv")
+        dfl = DataFrame(CSV.File(path, types=csvtypes,
+                                 dateformat=Dates.dateformat"mm/dd/yyyy HH:MM:SS"))
+        @info "Loaded data" path summary(dfl)
+        for (i,coltype) in enumerate(csvtypes)
+            if coltype == Float32
+                dfl[:, i] = max.(dfl[!, i], 0.0f0)
+            end
+        end
+        df = vcat(df, dfl)
+    end
+
+    goodwell_summary = @_ df |>
+        groupby(__, :API) |>
+        combine([:WellOil,:WellGas,:WellWater,:ReportDate]=>well_summary, __) |>
+        filter(_.WellOil_sum > 0 || _.WellGas_sum > 0, __)
+
+    startdate = minimum(skipmissing(goodwell_summary.StartDate))
+    enddate = maximum(skipmissing(goodwell_summary.EndDate))
+
+    dates = startdate:Dates.Month(1):enddate
+
+    return df, goodwell_summary, dates
+end
+
 
 end
